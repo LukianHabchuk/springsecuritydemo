6a9d904af87640bed3d48867b7a8fb6fea265956
Lukian Hrabchuk <lukian359@gmail.com>
2020-08-18 20:45:49 +0200

    authorization using user roles

---
+++ src/main/java/com/lukian/springsecuritydemo/model/Role.java
@@ -0,0 +1,6 @@
+package com.lukian.springsecuritydemo.model;
+
+public enum Role {
+    ADMIN,
+    USER
+}
--- src/main/java/com/lukian/springsecuritydemo/rest/DeveloperRestControllerV1.java
+++ src/main/java/com/lukian/springsecuritydemo/rest/DeveloperRestControllerV1.java
@@ -1,10 +1,7 @@
 package com.lukian.springsecuritydemo.rest;
 
 import com.lukian.springsecuritydemo.model.Developer;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 import java.util.stream.Collectors;
@@ -29,5 +26,16 @@ public class DeveloperRestControllerV1 {
         return DEVELOPERS.stream().filter(developer -> developer.getId().equals(id))
                 .findFirst()
                 .orElse(null);
+    }
+
+    @PostMapping
+    public Developer create(@RequestBody Developer developer) {
+        this.DEVELOPERS.add(developer);
+        return developer;
+    }
+
+    @DeleteMapping
+    public void deleteById(Long id) {
+        this.DEVELOPERS.removeIf(developer -> developer.getId().equals(id));
     }
 }
